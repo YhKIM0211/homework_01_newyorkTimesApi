db6beecbaf3c321c49e8cc4cdb97a5b8515f54d1
YhKIM0211 <kyh970211@ajou.ac.kr>
2022-06-13 19:13:41 +0900

    feat : header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Clip () {
+function Home () {
 
   return (
     <>
-      <h1>Clip</h1>
+      <h1>Home</h1>
     </>
   );
 };
 
-export default Clip;
+export default Home;
